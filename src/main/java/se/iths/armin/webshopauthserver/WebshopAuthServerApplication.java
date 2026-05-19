298af3e0586e64e9ec99e54359174d86c4e5af6e
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-19 10:51:18 +0200

    Initial commit

---
+++ src/main/java/se/iths/armin/webshopauthserver/WebshopAuthServerApplication.java
@@ -0,0 +1,13 @@
+package se.iths.armin.webshopauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/armin/webshopauthserver/WebshopAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.armin.webshopauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
