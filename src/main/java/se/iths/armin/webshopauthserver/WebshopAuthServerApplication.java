ffe0c593d4ccc88034e1aa172961ceb3a718abbe
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-19 10:59:43 +0200

    f

--- src/main/java/se/iths/armin/webshopauthserver/WebshopAuthServerApplication.java
+++ src/main/java/se/iths/armin/webshopauthserver/WebshopAuthServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebshopAuthServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebshopAuthServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopAuthServerApplication.class, args);
+    }
 
 }
